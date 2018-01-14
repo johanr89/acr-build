f3e1d50ed1837ca01625744a2ab4fcfb33621595
root <root@localhost.localdomain>
2018-01-13 22:23:57 -0500

    colour

--- add-ACR_kickstart.sh
+++ add-ACR_kickstart.sh
@@ -82,35 +82,63 @@ esac
 ##           MAIN
 ##
 ##########################################################
+clear
+PrintMsg yellow "\n\nACR KICKSTART INFO COLLECTOR\t[" ; PrintMsg normal " $0 " ; PrintMsg yellow "] \n"
+    PrintMsg normal "\n *  Collect a single ACR host's Kickstart configuration.\n"
+    PrintMsg normal "\n *  Serves as the primary input for the build-ISO-Tool.sh\n"
 
-PrintMsg yellow "\nINFO\n\n\t$0"
-PrintMsg red    "\tCollect a single ACR host's Kickstart configuration, to build the ISO\n"
-PrintMsg normal "\n\t1st run will offer the minimum config to enter."
-PrintMsg normal "\n\t2nd will offer all the more detailed, and assumed configuration."
-PrintMsg normal "\n\t... repeated thereafter, or until you ACCEPT\n"
-PrintMsg yellow "\n---------------------------------------------------------------------------"
-PrintMsg normal "\n"
+PrintMsg red "\n\n\n\t The key values, then more, if needed:"
+PrintMsg normal "\n\t---------------------------------------"
+    PrintMsg yellow  "\n\t 1st run "; PrintMsg normal " Minimal config, usually differs between hosts and installs." 
+    PrintMsg yellow  "\n\t 2nd run "; PrintMsg normal " All configuration, including 1st run. Minimal usually serves the need."
+
+PrintMsg red "\n\n\n\t Useful Tips:"
+PrintMsg normal "\n\t--------------"
+    PrintMsg yellow "\n\t defaults "; PrintMsg normal "When adding many similar hosts, change the defautls file first."
+        PrintMsg normal "\n\t          Defaults here: "; PrintMsg blue "$File_Kickstart_Defaults\n"
+    PrintMsg green "\n\t input "; PrintMsg normal "Limitation on input: "; PrintMsg red "NO" 
+        PrintMsg normal " spces (\" \") !\n\t       Use unserscore (\"_\") if you have too."
+
+PrintMsg normal   "\n\n\n\t#  github \""; PrintMsg blue "https://github.com/johanr89/acr-build" ; PrintMsg normal "\" "
+PrintMsg normal   "\n\t#  Open source, use at your own risk. This has no software or OS vendor, no guarentees."
+
+PrintMsg normal "\n\n\nPress ["
+PrintMsg yellow "Enter"
+PrintMsg normal "] to start.";read
+
+##########################################################
+
 
 if [ -f $MyTemplates/$File_Kickstart_Defaults ]
 then
+
 
     CountMax=`grep -v ^# $MyTemplates/$File_Kickstart_Defaults | grep \= | wc -l`
     grep -v ^# $MyTemplates/$File_Kickstart_Defaults | sort -n  > $Temp
 
     while [ `echo $Confirm | grep -i "y" | wc -l` -lt 1 ] 2>/dev/null
     do
+        clear
+        PrintMsg normal "\n"
+
+        if [ $IsThisTheFirst = "yes" ]
+        then
+            PrintMsg normal "run [" ; PrintMsg yellow " 1st essential only " ; PrintMsg normal "] \n"
+        else
+            PrintMsg normal "run [" ; PrintMsg yellow " Now all config values  " j PrintMsg normal "] \n"
+        fi
 	
-	if [ $IsThisTheFirst = "yes" ] && [ `find $MyDir -maxdepth 2 -type f -ctime +1 -iname "Host_Config_KS*.cfg" | wc -l` -gt 0 ]
-	then
+    	if [ $IsThisTheFirst = "yes" ] && [ `find $MyDir -maxdepth 2 -type f -ctime +1 -iname "Host_Config_KS*.cfg" | wc -l` -gt 0 ]
+    	then
             printf "\nFound older kickstart files:\n\n"
             find $MyDir -maxdepth 2 -type f -ctime +1 -name "Host_Config_KS*.cfg" | sed -e 's/^/\t/g'
             printf "\nRemove [y/n] ? "
-	    read ConfRemove
+    	    read ConfRemove
             [ $ConfRemove = "y" ] && find $MyDir -maxdepth 2 -type f -ctime +1 -name "Host_Config_KS*.cfg" -exec /bin/rm -i '{}' \; 
-	fi
+    	fi
 
-	for Counter in `seq -w $CountMin 1 $CountMax`
-	do
+    	for Counter in `seq -w $CountMin 1 $CountMax`
+    	do
             VarLine=`grep ^$Counter $Temp`
             VarPriority=`echo $VarLine | cut -f1 -d \:`
             VarKey=`echo $VarLine | cut -f1 -d \= | cut -f2 -d \:`
@@ -121,8 +149,17 @@ then
     	    then
                 NewVal=$VarDefault
             else
-                printf "\n$VarKey\t\"$VarText\" [$VarDefault] : "
+                PrintMsg normal "\n\"$VarKey\"\t"
+                PrintMsg green    "["
+                PrintMsg normal  "$VarDefault"
+                PrintMsg green    "]"
+                PrintMsg normal  "\t\tNote \"$VarText\""
+                PrintMsg normal "\nType a new value, "
+                PrintMsg green  "Enter"
+                PrintMsg normal " to accept : "
+                  
                 read NewCurrent
+
                 if [ `echo $NewCurrent | wc -c` -gt 1 ] 2>/dev/null
                 then
                     NewVal=$NewCurrent
@@ -133,30 +170,29 @@ then
 
             printf "$Counter:$VarKey=$NewVal=$VarText\n" >> $NewT
 
-	done
+    	done
 
-
-	PrintMsg red "\n\nPlease check and confirm:\n\n"
+    	PrintMsg red "\n\nPlease check and confirm:\n\n"
     
-    Lcol="normal" ; Ccol="blue" ; Rcol="yellow"
-    while read Line
-    do
-        PrintMsg $Lcol "\t`echo $Line | cut -c4-999 | cut -f1 -d \=`"
-        [ `echo $Line | cut -c4-999 | cut -f1 -d \= | wc -c` -le 8 ] && PrintMsg $Ccol "\t"
-        PrintMsg $Ccol "\t=\t[ "
-        PrintMsg $Rcol "`echo $Line | cut -c4-999 | cut -f2 -d \=`"
-        PrintMsg $Ccol " ]"
+        Lcol="normal" ; Ccol="blue" ; Rcol="normal"
+        while read Line
+        do
+            PrintMsg $Lcol "\t`echo $Line | cut -c4-999 | cut -f1 -d \=`"
+            [ `echo $Line | cut -c4-999 | cut -f1 -d \= | wc -c` -le 8 ] && PrintMsg $Ccol "\t"
+            PrintMsg $Ccol "\t=\t["
+            PrintMsg $Rcol "`echo $Line | cut -c4-999 | cut -f2 -d \=`"
+            PrintMsg $Ccol "]"
+            PrintMsg normal "\n"
+
+        done<$NewT
+
+    	PrintMsg red "\nAccept ?" ; PrintMsg yellow " [y/n] "
+
+    	read Confirm
+
         PrintMsg normal "\n"
-
-	done<$NewT
-
-	PrintMsg red "\nAccept ?"
-	PrintMsg yellow " [y/n] "
-	PrintMsg red "\t\"n\" will enable you to change all additional fields"
-	read Confirm
-    PrintMsg normal "\n"
-    IsThisTheFirst="no"
-	mv $NewT $Temp
+        IsThisTheFirst="no"
+        mv $NewT $Temp
         	
     done
 
